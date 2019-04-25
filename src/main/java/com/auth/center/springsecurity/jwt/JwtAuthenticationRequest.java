3366021cf500117d50d4b17a43ee975412a58bc4
李强强 <liqiangqiang@baiwang.com>
2019-04-25 10:14:27 +0800

    db

--- src/main/java/com/auth/center/springsecurity/jwt/JwtAuthenticationRequest.java
+++ src/main/java/com/auth/center/springsecurity/jwt/JwtAuthenticationRequest.java
@@ -9,10 +9,6 @@ public class  JwtAuthenticationRequest implements Serializable {
     private String username;
     private String password;
 
-    public JwtAuthenticationRequest() {
-        super();
-    }
-
     public JwtAuthenticationRequest(String username, String password) {
         this.setUsername(username);
         this.setPassword(password);
